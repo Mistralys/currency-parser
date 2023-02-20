9c3f9ef4b6ac88c9e72f4b9f13f15ef4e5849b9f
Mistralys <s.mordziol@gmail.com>
2023-02-20 15:59:35 +0100

    Implemented the base logic.

--- src/PriceFormatter.php
+++ src/PriceFormatter.php
@@ -6,111 +6,226 @@ namespace Mistralys\CurrencyParser;
 
 class PriceFormatter
 {
-    public const SPACE_PLACEHOLDER = '_space_';
-    public const SYMBOL_POSITION_FRONT = 'front';
-    public const SYMBOL_POSITION_END = 'end';
-    private string $nonBreakingSpace = '&#160;';
+    public const ERROR_INVALID_SYMBOL_MODE = 129701;
+    public const ERROR_INVALID_SYMBOL_POSITION = 129702;
 
-    private PriceMatch $price;
+    public const SPACE_PLACEHOLDER = '_space_';
+    public const SYMBOL_POSITION_BEFORE_MINUS = 'before-minus';
+    public const SYMBOL_POSITION_AFTER_MINUS = 'after-minus';
+    public const SYMBOL_POSITION_END = 'end';
+    public const SYMBOL_MODE_PRESERVE = 'preserve';
+    public const SYMBOL_MODE_NAME = 'name';
+    public const SYMBOL_MODE_SYMBOL = 'symbol';
+    public const SYMBOL_POSITIONS = array(
+        self::SYMBOL_POSITION_END,
+        self::SYMBOL_POSITION_AFTER_MINUS,
+        self::SYMBOL_POSITION_BEFORE_MINUS
+    );
+    public const SYMBOL_MODES = array(
+        self::SYMBOL_MODE_NAME,
+        self::SYMBOL_MODE_PRESERVE,
+        self::SYMBOL_MODE_SYMBOL
+    );
+
+    private string $nonBreakingSpace = '&#160;';
+    private PriceMatch $workPrice;
     private string $decimalSeparator;
     private string $thousandsSeparator;
     private string $arithmeticSeparator;
-    private bool $financial = false;
-    private string $symbolPosition = self::SYMBOL_POSITION_FRONT;
+    private bool $symbolSpaceEnabled = false;
+    private string $symbolPosition = self::SYMBOL_POSITION_AFTER_MINUS;
+    private string $symbolMode = self::SYMBOL_MODE_PRESERVE;
 
-    public function __construct(PriceMatch $price, string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator)
+    private function __construct(string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator)
     {
-        $this->price = $price;
         $this->decimalSeparator = $decimalSeparator;
         $this->thousandsSeparator = $thousandsSeparator;
         $this->arithmeticSeparator = $arithmeticSeparator;
     }
 
-    public function makeFinancial() : self
+    public static function create(string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator='') : PriceFormatter
     {
-        $this->financial = true;
-        return $this;
+        return new PriceFormatter($decimalSeparator, $thousandsSeparator, $arithmeticSeparator);
     }
 
+    // region: A - Utility methods
+
+    /**
+     * @param string $position
+     * @return $this
+     * @throws PriceFormatterException {@see self::ERROR_INVALID_SYMBOL_POSITION}
+     */
     public function setSymbolPosition(string $position) : self
     {
-        $this->symbolPosition = $position;
-        return $this;
-    }
+        if(in_array($position, self::SYMBOL_POSITIONS)) {
+            $this->symbolPosition = $position;
+            return $this;
+        }
 
-    public function formatR(string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator) : string
-    {
-        return str_replace(
-            PriceFormatter::SPACE_PLACEHOLDER,
-            $this->nonBreakingSpace,
-            $this->renderRegular($decimalSeparator, $thousandsSeparator, $arithmeticSeparator)
+        throw new PriceFormatterException(
+            'Invalid price formatter symbol position.',
+            sprintf(
+                'The position [%s] is unknown. Valid positions are: [%s].',
+                $position,
+                implode(', ', self::SYMBOL_POSITIONS)
+            ),
+            self::ERROR_INVALID_SYMBOL_POSITION
         );
     }
 
-    public function formatFinancial() : string
+    public function setSymbolSpaceEnabled(bool $enabled=true) : self
     {
-
+        $this->symbolSpaceEnabled = $enabled;
+        return $this;
     }
 
-    private function renderRegular(string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator) : string
+    /**
+     * @param string $mode
+     * @return $this
+     * @throws PriceFormatterException {@see self::ERROR_INVALID_SYMBOL_MODE}
+     */
+    public function setSymbolMode(string $mode) : self
     {
-        return $this->spaceFront.
-            $this->renderSign($arithmeticSeparator).
-            $this->renderNumber($this->number, $thousandsSeparator).
-            $this->renderDecimals($decimalSeparator).
-            $this->renderCurrency().
+        if(in_array($mode, self::SYMBOL_MODES)) {
+            $this->symbolMode = $mode;
+            return $this;
+        }
+
+        throw new PriceFormatterException(
+            'Invalid price formatter symbol mode.',
+            sprintf(
+                'The mode [%s] is unknown. Valid modes are: [%s].',
+                $mode,
+                implode(', ', self::SYMBOL_MODES)
+            ),
+            self::ERROR_INVALID_SYMBOL_MODE
+        );
+    }
+
+    public function formatPrice(PriceMatch $price) : string
+    {
+        $this->workPrice = $price;
+
+        $result = str_replace(
+            self::SPACE_PLACEHOLDER,
+            $this->nonBreakingSpace,
+            $this->render()
+        );
+
+        unset($this->workPrice);
+
+        return $result;
+    }
+
+    // endregion
+
+    // region: Rendering
+
+    private function render() : string
+    {
+        return
+            $this->workPrice->getSpaceFront().
+            $this->renderSymbolBeforeMinus().
+            $this->renderSign().
+            $this->renderSymbolAfterMinus().
+            $this->renderNumber().
+            $this->renderDecimals().
+            $this->renderSymbolEnd().
             $this->renderVAT().
-            $this->spaceEnd;
+            $this->workPrice->getSpaceEnd();
     }
 
-    private function renderFinancial(string $decimalSeparator, string $thousandsSeparator, string $arithmeticSeparator) : string
+    private function resolveSymbol() : string
     {
-        return $this->spaceFront.
-            $this->renderSign($arithmeticSeparator).
-            $this->renderNumber($this->number, $thousandsSeparator).
-            $this->renderDecimals($decimalSeparator).
-            $this->renderCurrency().
-            $this->renderVAT().
-            $this->spaceEnd;
+        if($this->symbolMode === self::SYMBOL_MODE_NAME) {
+            return $this->workPrice->getCurrencyName();
+        }
+
+        if($this->symbolMode === self::SYMBOL_MODE_SYMBOL) {
+            return $this->workPrice->getCurrency()->getSymbol();
+        }
+
+        return $this->workPrice->getMatchedCurrencySymbol();
     }
 
-    private function renderCurrency() : string
+    private function resolveSymbolSpace() : string
     {
-        return PriceFormatter::SPACE_PLACEHOLDER .$this->currency;
+        if($this->symbolSpaceEnabled) {
+            return self::SPACE_PLACEHOLDER;
+        }
+
+        return '';
+    }
+
+    private function renderSymbolBeforeMinus() : string
+    {
+        if($this->symbolPosition === self::SYMBOL_POSITION_BEFORE_MINUS)
+        {
+            return $this->resolveSymbol() . $this->resolveSymbolSpace();
+        }
+
+        return '';
+    }
+
+    private function renderSymbolAfterMinus() : string
+    {
+        if($this->symbolPosition === self::SYMBOL_POSITION_AFTER_MINUS)
+        {
+            return $this->resolveSymbolSpace() . $this->resolveSymbol();
+        }
+
+        return '';
+    }
+
+    private function renderSymbolEnd() : string
+    {
+        if($this->symbolPosition === self::SYMBOL_POSITION_END)
+        {
+            return $this->resolveSymbolSpace() . $this->resolveSymbol();
+        }
+
+        return '';
     }
 
     private function renderVAT() : string
     {
-        if(!empty($this->vat))
+        if($this->workPrice->hasVAT())
         {
-            return PriceFormatter::SPACE_PLACEHOLDER .$this->vat;
+            return self::SPACE_PLACEHOLDER .$this->workPrice->getVAT();
         }
 
         return '';
     }
 
-    private function renderSign(string $arithmeticSeparator) : string
+    private function renderSign() : string
     {
-        if(!empty($this->sign))
+        if($this->workPrice->isNegative())
         {
-            return $this->sign.$arithmeticSeparator;
+            return '-'.$this->arithmeticSeparator;
         }
 
         return '';
     }
 
-    private function renderDecimals(string $decimalSeparator) : string
+    private function renderDecimals() : string
     {
-        if(!empty($this->decimals))
+        if($this->workPrice->hasDecimals())
         {
-            return $decimalSeparator.$this->decimals;
+            return $this->decimalSeparator.$this->workPrice->getDecimals();
         }
 
         return '';
     }
 
-    private function renderNumber(int $number, string $thousandsSeparator) : string
+    private function renderNumber() : string
     {
-        return number_format($number, 0, '.', $thousandsSeparator);
+        return number_format(
+            $this->workPrice->getNumber(),
+            0,
+            '.',
+            $this->thousandsSeparator
+        );
     }
+
+    // endregion
 }
