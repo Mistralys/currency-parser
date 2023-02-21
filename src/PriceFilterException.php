66a82adf8b89db1a7efc5897b8cf966ebfe04744
Mistralys <s.mordziol@gmail.com>
2023-02-21 11:25:21 +0100

    Create PriceFilterException.php

---
+++ src/PriceFilterException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\CurrencyParser;
+
+use Mistralys\Rygnarok\Newsletter\CharFilter\CurrencyParserException;
+
+class PriceFilterException extends CurrencyParserException
+{
+
+}
