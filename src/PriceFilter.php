b7bcc89b5f675dd7f2ea675f4da9d0941b14d095
Mistralys <s.mordziol@gmail.com>
2023-02-21 11:25:18 +0100

    Revamped to support multiple currencies.

--- src/PriceFilter.php
+++ src/PriceFilter.php
@@ -5,36 +5,87 @@ declare(strict_types=1);
 namespace Mistralys\CurrencyParser;
 
 use AppUtils\FileHelper\FileInfo;
+use Mistralys\Rygnarok\Newsletter\CharFilter\CurrencyParserException;
+use function AppUtils\parseVariable;
 
 class PriceFilter
 {
-    private PriceFormatter $formatter;
+    public const ERROR_INVALID_CURRENCY = 129801;
+
+    /**
+     * @var PriceFormatter[]
+     */
+    private array $formatters = array();
+
     private PriceParser $parser;
 
-    private function __construct(PriceFormatter $formatter, PriceParser $parser)
+    private function __construct(PriceParser $parser)
     {
-        $this->formatter = $formatter;
         $this->parser = $parser;
     }
 
-    public static function create(PriceFormatter $formatter, PriceParser $parser) : PriceFilter
+    /**
+     * Creates a filter for an existing parser instance: will format
+     * all currencies that the parser is configured for.
+     *
+     * @param PriceParser $parser
+     * @return PriceFilter
+     */
+    public static function create(PriceParser $parser) : PriceFilter
     {
-        return new PriceFilter($formatter, $parser);
+        return new PriceFilter($parser);
     }
 
     /**
-     * @param string|BaseCurrency $currency
-     * @param PriceFormatter $formatter
+     * Creates a filter for the specified currencies. The parser
+     * instance is created automatically for the currencies.
+     *
+     * @param string|BaseCurrency ...$currencies Currency names (e.g. "USD") or currency instances.
      * @return PriceFilter
+     * @throws CurrencyParserException
      */
-    public static function createForCurrency($currency, PriceFormatter $formatter) : PriceFilter
+    public static function createForCurrencies(...$currencies) : PriceFilter
     {
         return self::create(
-            $formatter,
-            PriceParser::create()->expectCurrency($currency)
+            PriceParser::create()
+                ->expectCurrencies(...$currencies)
         );
     }
 
+    /**
+     * Sets a specific formatter to use for the target currency.
+     *
+     * @param string|BaseCurrency $currencyNameOrInstance
+     * @param PriceFormatter $formatter
+     * @return $this
+     * @throws PriceFilterException
+     */
+    public function setFormatter($currencyNameOrInstance, PriceFormatter $formatter) : self
+    {
+        $currency = Currencies::getInstance()->resolveCurrency($currencyNameOrInstance);
+
+        if($currency !== null) {
+            $this->formatters[$currency->getName()] = $formatter;
+            return $this;
+        }
+
+        throw new PriceFilterException(
+            'Invalid currency specified.',
+            sprintf(
+                'Could not determine the currency to use. Given: [%s].',
+                parseVariable($currencyNameOrInstance)->enableType()->toString()
+            ),
+            self::ERROR_INVALID_CURRENCY
+        );
+    }
+
+    /**
+     * @param string $subject
+     * @return string
+     * @throws PriceFilterException
+     * @throws PriceFormatterException
+     * @throws CurrencyParserException
+     */
     public function filterString(string $subject) : string
     {
         $prices = $this->parser->findPrices($subject);
@@ -42,7 +93,7 @@ class PriceFilter
 
         foreach($prices as $price)
         {
-            $replaces[$price->getMatchedString()] = $this->formatter->formatPrice($price);
+            $replaces[$price->getMatchedString()] = $this->resolveFormatter($price)->formatPrice($price);
         }
 
         return str_replace(
@@ -50,6 +101,32 @@ class PriceFilter
             array_values($replaces),
             $subject
         );
+    }
+
+    public function getFormatter(BaseCurrency $currency) : ?PriceFormatter
+    {
+        return $this->formatters[$currency->getName()] ?? null;
+    }
+
+    /**
+     * @param PriceMatch $price
+     * @return PriceFormatter
+     * @throws PriceFilterException
+     * @throws PriceFormatterException
+     */
+    private function resolveFormatter(PriceMatch $price) : PriceFormatter
+    {
+        $currency = $price->getCurrency();
+        $formatter = $this->getFormatter($currency);
+
+        if($formatter !== null) {
+            return $formatter;
+        }
+
+        $formatter = PriceFormatter::createForLocale($currency->getDefaultLocale());
+        $this->setFormatter($currency, $formatter);
+
+        return $formatter;
     }
 
     public function filterFile(FileInfo $file) : string
