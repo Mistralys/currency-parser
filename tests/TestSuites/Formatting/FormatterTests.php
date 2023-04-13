7bc5d94581303bd8e7bc306bb4f4d3320aeb2d7f
Mistralys <s.mordziol@gmail.com>
2023-04-13 08:11:19 +0200

    Added a test.

--- tests/TestSuites/Formatting/FormatterTests.php
+++ tests/TestSuites/Formatting/FormatterTests.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mistralys\CurrencyParserTests\TestSuites\Formatting;
 
+use Mistralys\CurrencyParser\PriceFilter;
 use Mistralys\CurrencyParser\PriceFormatter;
 use Mistralys\CurrencyParserTests\TestClasses\CurrencyParserTestCase;
 
@@ -85,5 +86,24 @@ final class FormatterTests extends CurrencyParserTestCase
                 ->setNonBreakingSpace('[SPACE]')
                 ->format()
         );
+    }
+
+    public function test_formatHTML() : void
+    {
+        $html = <<<'EOT'
+<p>Symbol on the wrong side: <strong>&#8364; 50</strong></p>
+<p>Name on the wrong side: <strong>EUR 50</strong></p>
+<p>Wrong thousands and decimal separators: <strong>1.000,00 &#8364;</strong></p>
+<p>No thousands separator: <strong>1000.00 &#8364;</strong></p>
+EOT;
+
+        $expected = <<<'EOT'
+<p>Symbol on the wrong side: <strong>50 &#8364;</strong></p>
+<p>Name on the wrong side: <strong>50 EUR</strong></p>
+<p>Wrong thousands and decimal separators: <strong>1.000,00 &#8364;</strong></p>
+<p>No thousands separator: <strong>1.000,00 &#8364;</strong></p>
+EOT;
+
+        $this->assertSame($expected, PriceFilter::createForLocales('EUR_DE')->filterString($html));
     }
 }
